580a61a8578b57d10a61a55ac523af81e47cbd2c
Eedrah <eedrah@users.noreply.github.com>
2016-09-23 04:41:10 -0600

    create back-to-back-practice.js

---
+++ back-to-back-practice.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Duolingo Back To Back Practice
+// @description  Starts another practice when one ends
+// @author       Eedrah
+// @namespace    https://github.com/eedrah/duolingo-scripts/raw/master/back-to-back-practice.js
+// @match        https://www.duolingo.com/practice*
+// @downloadURL  https://github.com/eedrah/duolingo-scripts/raw/master/back-to-back-practice.js
+// @version      1.0.0
+// ==/UserScript==
+
+var textOfInterest = [
+  'Practice again',
+  'Practice without a timer'
+]
+
+function findButtonOfInterest () {
+  var buttons = document.querySelectorAll('button')
+  var buttonsOfInterest = Array.prototype.filter.call(buttons, function (button) {
+    return textOfInterest.indexOf(button.textContent) !== -1
+  })
+  return buttonsOfInterest[0]
+}
+
+new MutationObserver(function (nodes) { // eslint-disable-line no-undef
+  findButtonOfInterest().click()
+}).observe(document.querySelector('body'), {
+  childList: true
+})
