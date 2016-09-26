54b10ec65b9a1707d3877e8cd6645b4a1aebdf87
Eedrah <eedrah@users.noreply.github.com>
2016-09-26 00:36:00 -0600

    implement a toggle

--- back-to-back-practice.user.js
+++ back-to-back-practice.user.js
@@ -3,17 +3,31 @@
 // @description  Starts another practice when one ends
 // @author       Eedrah
 // @namespace    https://github.com/eedrah/duolingo-scripts/raw/master/back-to-back-practice.user.js
-// @match        https://www.duolingo.com/*
 // @downloadURL  https://github.com/eedrah/duolingo-scripts/raw/master/back-to-back-practice.user.js
-// @version      1.3.0
+// @match        https://www.duolingo.com/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @version      2.0.0
 // ==/UserScript==
 
-var textOfInterest = [
-  'Practice again',
-  'Practice without a timer'
-]
+/* globals GM_getValue GM_setValue */
+
+var ContinuousPractice = {
+  get: function () {
+    return GM_getValue('continuous_practice')
+  },
+  set: function (value) {
+    GM_setValue('continuous_practice', value)
+    updateGui()
+    ifNotBlockedByUserExecute(runContinuousPractice)
+  }
+}
 
 function findButtonOfInterest () {
+  var textOfInterest = [
+    'Practice again',
+    'Practice without a timer'
+  ]
   var buttons = document.querySelectorAll('button')
   var buttonsOfInterest = Array.prototype.filter.call(buttons, function (button) {
     return textOfInterest.indexOf(button.textContent) !== -1
@@ -21,39 +35,70 @@ function findButtonOfInterest () {
   return buttonsOfInterest[0]
 }
 
-function hideProgressBar () {
-  var progressBar = document.querySelector('#progress-bar')
-  if (progressBar) {
-    progressBar.style.display = 'none'
-  }
-}
-
 function isGeneralPractice () {
   return window.location.pathname === '/practice'
 }
 
-function runIfNotBlockedByUser (callback) {
-  var shiftKeyIsPressed
-  function detectShiftKey (event) {
-    shiftKeyIsPressed = event.shiftKey
+function ifNotBlockedByUserExecute (callback) {
+  if (ContinuousPractice.get()) {
+    callback()
+  }
+}
+
+function displaySettings () {
+  var container = document.querySelector('.player-header-left')
+  if (!container) {
+    return
   }
 
-  window.addEventListener('keydown', detectShiftKey)
-  setTimeout(function () {
-    window.removeEventListener('keydown', detectShiftKey)
-    if (!shiftKeyIsPressed) {
-      callback()
+  var settingsElement = document.createElement('li')
+  var p = document.createElement('p')
+  p.classList.add('player-quit')
+  p.classList.add('gray')
+  p.classList.add('gray-text')
+  var label = document.createElement('label')
+  var input = document.createElement('input')
+  input.classList.add('border')
+  input.setAttribute('type', 'checkbox')
+  var text = document.createTextNode('Continuous practice')
+
+  if (ContinuousPractice.get()) {
+    input.setAttribute('checked', 'checked')
+  }
+  input.addEventListener('change', function () {
+    ContinuousPractice.set(!ContinuousPractice.get())
+  })
+
+  label.appendChild(input)
+  label.appendChild(text)
+  p.appendChild(label)
+  settingsElement.appendChild(p)
+
+  container.innerHTML = ''
+  container.appendChild(settingsElement)
+}
+
+function runContinuousPractice () {
+  var button = findButtonOfInterest()
+  button && button.click()
+}
+
+function updateGui () {
+  displaySettings()
+  var progressBar = document.querySelector('#progress-bar')
+  if (progressBar) {
+    if (ContinuousPractice.get()) {
+      progressBar.style.display = 'none'
+    } else {
+      progressBar.style.display = ''
     }
-  }, 100)
+  }
 }
 
 new MutationObserver(function (nodes) { // eslint-disable-line no-undef
   if (isGeneralPractice()) {
-    runIfNotBlockedByUser(function () {
-      hideProgressBar()
-      var button = findButtonOfInterest()
-      button && button.click()
-    })
+    updateGui()
+    ifNotBlockedByUserExecute(runContinuousPractice)
   }
 }).observe(document.querySelector('body'), {
   childList: true
